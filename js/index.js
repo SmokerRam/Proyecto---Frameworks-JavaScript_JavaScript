81cebce23c849365e1d30dc8c4902ec69a5a591c
SmokerRam <angel.raminez01@gmail.com>
2021-11-09 19:11:22 -0600

    Events Cards Index

--- js/index.js
+++ js/index.js
@@ -2,33 +2,141 @@
 
 //Define las variables que necesites
 
+var proximos = [];
+var pasados = [];
+var extraidosProximos = [];
+var extraidosPasados = [];
+
+var eventos, hoy, longitud;
+
+var i = 0;
+var o = 0;
+var ultimoNumeroProximo = -1;
+var ultimoNumeroPasado = -1;
 
 $(document).ready(function () {
 
   //Carga los datos que estan en el JSON (info.json) usando AJAX
+  $.ajax({
+    url: "info.json"
+  }).done(function(resultado){
+    //Guarda el resultado en variables
+    hoy = resultado.fechaActual;
+    eventos = resultado.eventos;
 
-  //Guarda el resultado en variables
+    //Clasifica los eventos segun la fecha actual del JSON
+    for(var x of eventos){
+      if(x.fecha > hoy){
+        proximos.push(x);
+      }
+      else{
+        pasados.push(x);
+      }
+    }
 
-  //Clasifica los eventos segun la fecha actual del JSON
+    //<!--==================== EVENTOS PROXIMOS ====================-->
+    //Ordena los eventos segun la fecha (los mas cercanos primero)
+    proximos = proximos.sort(function(x,y){
+      if(x.fecha < y.fecha){
+        return 1;
+      }
+      return -1;
+    });
 
-  //Ordena los eventos segun la fecha (los mas cercanos primero)
+    //Extrae solo dos eventos proximos
+    while(i < 2){
+      var randomProximo = Math.floor(Math.random() * proximos.length)
+      
+      if(ultimoNumeroProximo != randomProximo){
+        extraidosProximos.push(proximos[randomProximo])
+        ultimoNumeroProximo = randomProximo;
+        i++; 
+      }    
+    }
 
-  //Extrae solo dos eventos
+    //Ordena los eventos extraidos segun la fecha (los mas cercanos primero)
+    extraidosProximos = extraidosProximos.sort(function(x,y){
+      if(x.fecha < y.fecha){
+        return 1;
+      }
+      return -1;
+    });
 
-  //Ordena los eventos segun la fecha (los mas cercanos primero)
+    //Crea un string que contenga el HTML que describe el detalle del evento
 
-  //Extrae solo dos eventos
+    var htmlProximo = "";
 
-  //Crea un string que contenga el HTML que describe el detalle del evento
+    //Recorre el arreglo y concatena el HTML para cada evento
 
-  //Recorre el arreglo y concatena el HTML para cada evento
+    for(var x of extraidosProximos){
+      htmlProximo += `
+        <div class="col-12 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">${x.nombre}</h3>
+                    <small class="text-muted">${x.fecha}</small>
+                    <p class="card-text">${x.descripcion}</p>
+                </div>
+            </div>
+        </div>
+      `
+    }
 
-  //Modifica el DOM agregando el html generado
+    //Modifica el DOM agregando el html generado
+    document.getElementById("proximos").innerHTML = htmlProximo;
 
-  //Crea un string que contenga el HTML que describe el detalle del evento
 
-  //Recorre el arreglo y concatena el HTML para cada evento
+    //<!--==================== EVENTOS PASADOS ====================-->
 
-  //Modifica el DOM agregando el html generado
+    //Ordena los eventos segun la fecha (los mas cercanos primero)
+    pasados = pasados.sort(function(x,y){
+      if(x.fecha < y.fecha){
+        return 1;
+      }
+      return -1;
+    })
+
+    //Extrae solo dos eventos pasados
+    while(o < 2){
+      var randomPasado = Math.floor(Math.random() * pasados.length)
+
+      if(ultimoNumeroPasado != randomPasado){
+        extraidosPasados.push(pasados[randomPasado])
+        ultimoNumeroPasado = randomPasado;
+        o++;
+      }
+    }
+
+    //Ordena los eventos extraidos segun la fecha (los mas cercanos primero)
+    extraidosPasados = extraidosPasados.sort(function(x,y){
+      if(x.fecha > y.fecha){
+        return 1;
+      }
+      return -1;
+    });
+
+    //Crea un string que contenga el HTML que describe el detalle del evento
+
+    var htmlPasado = ""
+
+    //Recorre el arreglo y concatena el HTML para cada evento
+
+    for(var x of extraidosPasados){
+      htmlPasado += `
+        <div class="col-12 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">${x.nombre}</h3>
+                    <small class="text-muted">${x.fecha}</small>
+                    <p class="card-text">${x.descripcion}</p>
+                </div>
+            </div>
+        </div>
+      `
+    }
+
+    //Modifica el DOM agregando el html generado
+    document.getElementById("pasados").innerHTML = htmlPasado;
+  })
 
 });
