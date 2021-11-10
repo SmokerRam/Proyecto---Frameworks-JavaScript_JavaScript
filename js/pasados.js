bb1c12599c42a92e976e55f9deeaf7e189c339f3
SmokerRam <angel.raminez01@gmail.com>
2021-11-09 23:00:41 -0600

    Eventos Pasados

--- js/pasados.js
+++ js/pasados.js
@@ -1,19 +1,64 @@
+// Hemos omitido los acentos en los comentarios por compatibilidad
+
 //Define las variables que necesites
+
+var pasados = [];
+var extraidos = [];
+
+var eventos, hoy;
+
+var i = 0;
+var ultimoNumero = -1;
 
 $(document).ready(function () {
 
   //Carga los datos que estan en el JSON (info.json) usando AJAX
+  $.ajax({
+    url: "info.json"
+  }).done(function(resultado){
+    //Guarda el resultado en variables
+    hoy = resultado.fechaActual;
+    eventos = resultado.eventos;
 
-  //Guarda el resultado en variables
+    //Clasifica los eventos segun la fecha actual del JSON
+    for(var x of eventos){
+      if(x.fecha < hoy){
+        pasados.push(x);
+      }
+    }
 
-  //Selecciona los eventos que sean anteriores a la fecha actual del JSON
+    //Ordena los eventos segun la fecha (los mas cercanos primero)
+    pasados = pasados.sort(function(x,y){
+      if(x.fecha > y.fecha){
+        return 1;
+      }
+      return -1;
+    });
 
-  //Ordena los eventos segun la fecha (los mas recientes primero)
+    //Crea un string que contenga el HTML que describe el detalle del evento
 
-  //Crea un string que contenga el HTML que describe el detalle del evento
+    var html = "";
 
-  //Recorre el arreglo y concatena el HTML para cada evento
+    //Recorre el arreglo y concatena el HTML para cada evento
 
-  //Modifica el DOM agregando el html generado
+    for(var x of pasados){
+      html += `
+        <div class="col-12 col-md-12 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title p-0 m-0">${x.nombre}</h3>
+                    <small class="text-muted">${x.fecha} - ${x.lugar}</small>
+                    <p class="card-text pt-1">${x.descripcion}</p>
+                    Invitados: ${x.invitados}
+                </div>
+            </div>
+        </div>
+      `
+    }
+
+    //Modifica el DOM agregando el html generado
+    document.getElementById("pasados").innerHTML = html;
+
+  })
 
 });
